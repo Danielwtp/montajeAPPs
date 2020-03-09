aa2bf37bbfdfb9eab8f8f94ae81945089d93f8f4
Daniel Colón <daniel.colon@vipmac0007-2.local>
2020-03-09 18:52:11 +0100

    Quick save

---
+++ script.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import os, fileinput, sys
+from os import path, listdir
+
+
+#Check de parametros.
+dir = os.getcwd() + "/versiones/"
+proyectos = "["
+for i in listdir(dir):
+        proyectos += i + ", "
+
+if len(sys.argv) != 3:
+    print("./script.py " + proyectos[0:-2]  + "] [pre, stg]")
+    exit(1)
+
+if path.exists(dir+str(sys.argv[1]))==False:
+    print("./script.py " + proyectos[0:-2]  + "] [pre, stg]")
+    exit(1)
+
+if (str(sys.argv[2]) != "pre" and str(sys.argv[2]) != "stg"):
+    print("./script.py " + proyectos[0:-2]  + "] [pre, stg]")
+    exit(1)
+
+#Check de versiones.
+name = input("Ha cambiado alguna version de "+ str(sys.argv[1]) +"?(y/N)\n")
+while (name != "" and name != "y" and name != "n"):
+    name = input("Presiona y(si) or n(no) por favor\n")
+
+file = open(dir+str(sys.argv[1]))#close the file plz
+
+numero = 0
+if name == "y":
+    print("Cuales?:(Escribe los numeros separados por espacios.)")
+    for i in file:
+        print(str(numero) + ": " + i[0:-1])
+        numero = numero + 1
+    ids = input()
+    for j in ids: 
+        if j != " ":
+            if int(j[0]) >= numero:
+                print(j+" no pertenece a ninguna de las dependencias")
+                file.close()
+                exit(1)
+            
+    
